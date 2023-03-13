485dfde52e06c8581ec8cc228dbd84237f976289
swordsmanqaq <swords_man12@163.com>
2023-03-13 19:23:25 +0800

    前端代码

---
+++ src/main.js
@@ -0,0 +1,96 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+//import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import routes from './routes'
+//import NProgress from 'nprogress'
+
+//import 'nprogress/nprogress.css'
+// import Mock from './mock'
+// Mock.bootstrap();
+
+//导入axios
+import axios from "axios";
+
+axios.defaults.baseURL = '/api'  // http://localhost:8082/api
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+    routes
+})
+
+// router.beforeEach((to, from, next) => {
+//     //NProgress.start();
+//     if (to.path == '/login') {
+//         localStorage.removeItem('token');
+//     }
+//     let user = JSON.parse(localStorage.getItem('loginUser'));
+//     if (!user && to.path != '/login') {
+//         alert("请登录后再次访问!")
+//         next({path: '/login'})
+//     } else {
+//         next()
+//     }
+// })
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+//axios请求拦截器:添加请求拦截器，在请求头中加token
+//前端调用后端接口,是过axios发起请求的,在axios调用后端接口之前,都会先经过axios的前置拦截器
+axios.interceptors.request.use(
+    config => {
+        // 获取token
+        let token = localStorage.getItem("token");
+
+        // 有值就放到请求头中
+        if (token) {
+            config.headers.token = token;
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    })
+// 响应拦截器:后端响应结果给前端时,都要先经过响应拦截器
+axios.interceptors.response.use(function (response) {
+    //对返回的数据进行操作
+    let result = response.data;  //后端拦截器返回的数据
+    if (!result.success && result.message == "NoLogin") { //条件成立说明没有登录，需要拦截
+        router.push({path: '/login'});  // 跳转回登陆页面
+        return ElementUI.Message.error("请登录后访问");
+        // return response;
+    } else if (!result.success && result.message == "NoPermission"){
+        router.push({path: '/403'});  // 跳转403页面
+
+    } else {
+        // 如果已经登录过，就继续执行
+        return response;
+    }
+}, function (err) {
+    return Promise.reject(err)
+})
+
+
+new Vue({
+    //el: '#app',
+    //template: '<App/>',
+    router,
+    store,
+    //components: { App }
+    render: h => h(App)
+}).$mount('#app')
+
---
+++ src/views/Login.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-form :model="loginDto" :rules="rules2" ref="loginFrom" label-position="left" label-width="0px"
+           class="demo-ruleForm login-container">
+    <h3 class="title">系统登录</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="loginDto.username" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password" v-model="loginDto.password" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="loginIn" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+//import NProgress from 'nprogress'
+export default {
+  data() {
+    return {
+      logining: false,
+
+      loginDto: {
+        username: '',
+        password: ''
+      },
+      rules2: {
+        username: [
+          {required: true, message: '请输入账号', trigger: 'blur'},
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+        ]
+      },
+      checked: true
+    };
+  },
+  methods: {
+    handleReset2() {
+      this.$refs.loginFrom.resetFields();
+    },
+    loginIn(ev) {
+      var _this = this;
+      this.$refs.loginFrom.validate((valid) => {
+        if (valid) {
+          //_this.$router.replace('/table');
+          this.logining = true;
+          this.$http.post("/login", this.loginDto)
+              .then(result => {
+                result = result.data;
+                this.logining = false;
+                if (result.success) {
+                  this.$message({message: "登录成功", type: 'success'});
+                  //把后端返回的token和employee存储到localStorage中
+                  localStorage.setItem("token", result.resultObj.token);
+                  localStorage.setItem("loginUser", JSON.stringify(result.resultObj.loginUser));
+                  //跳转首页
+                  this.$router.push({path: '/echarts'});
+                } else {
+                  this.$message({message: result.message, type: "error"});
+                }
+              }).catch(result => {
+            this.$message({message: result.message, type: 'error'});
+          })
+
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+
+  .title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+  .remember {
+    margin: 0px 0px 35px 0px;
+  }
+}
+</style>
